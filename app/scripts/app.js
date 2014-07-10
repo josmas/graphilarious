7e55c7aa8b129b136c8f777678d8c9834aec1a8a
josmas <josmasflores@gmail.com>
2014-07-10 19:19:47 -0400

    Initial import from yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name graphilariousApp
+ * @description
+ * # graphilariousApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('graphilariousApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
