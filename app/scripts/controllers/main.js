698a9beee2bca6623113fce79886126e811069fa
josmas <josmasflores@gmail.com>
2014-07-11 15:25:50 -0400

    adds some more shapes and handlers to the svg objects

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,15 +17,36 @@ angular.module('graphilariousApp')
 
   var d3Sample = function (){
 
-    d3.select('#d3-container')
+    var svgContainer = d3.select('#d3-container')
       .append('svg')
-      .attr('width', 50)
-      .attr('height', 50)
-      .append('circle')
-      .attr('cx', 25)
-      .attr('cy', 25)
-      .attr('r', 25)
-      .style('fill', 'purple');
+      .attr('width', 500)
+      .attr('height', 500);
+
+    var node1 = svgContainer.append('circle')
+      .style('stroke', 'gray')
+      .style('fill', 'purple')
+      .attr('r', 40)
+      .attr('cx', 50)
+      .attr('cy', 50);
+
+    var node2 = svgContainer.append('circle')
+      .style('stroke', 'gray')
+      .style('fill', 'white')
+      .attr('r', 40)
+      .attr('cx', 250)
+      .attr('cy', 250)
+      .on('mouseover', function(){d3.select(this).style('fill', 'aliceblue');})
+      .on('mouseout', function(){d3.select(this).style('fill', 'white');});
+
+    var line1To2 = svgContainer.append('line')
+      .style('stroke', 'black')
+      .style('stroke-width', '4px')
+      .style('fill', 'black')
+      .attr('x1', 50)
+      .attr('y1', 50)
+      .attr('x2', 250)
+      .attr('y2', 250)
+      .on('click', function(){console.log('handle clicking on Edges.. coming soon!');});
   };
 
   d3Sample();
